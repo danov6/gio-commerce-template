3a944403681b1ed33ff836543bd2016f06c66c81
danov6 <glvaldez@callutheran.edu>
2023-09-13 19:35:13 -0700

    Pages
    Starter pages for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Category } from './pages/Category';
+import { Product } from './pages/Product';
+import { Cart } from './pages/Cart';
+import { Checkout } from './pages/Checkout';
 
 function App() {
   return (
-    <div className="page-wrapper">
-
-    </div>
+  <Routes>
+    <Route exact path="/" element={<Home />}/>
+    <Route path="/category" element={<Category />}/>
+    <Route path="/product" element={<Product />}/>
+    <Route path="/cart" element={<Cart />}/>
+    <Route path="/checkout" element={<Checkout />}/>
+  </Routes>
   );
 }
 
